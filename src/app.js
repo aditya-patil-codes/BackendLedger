26a13ec2ba4efb1b62ba91b927bd5569f70c14f3
Aditya Patil <aditya249645@gmail.com>
2026-08-30 20:38:06 +0530

    welcome message

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ const app = express();
 
 app.use(express.json()); // this is a middleware - by default the express server cant read the req.body data. so that we use that.
 app.use(cookieParser());
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the Ledger API");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/accounts", accountRouter)
 app.use("/api/transactions", transactionRoutes);
